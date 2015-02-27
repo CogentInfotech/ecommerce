28e4b44f759a741a22ec8252f5fff9f4f918334b
cogent2 <cogent2@CogentJava>
2015-02-27 16:46:50 -0500

    Adding AspectJ Support

---
+++ Ecommerce/src/main/java/com/ecommerce/aspect/LoggingAspect.java
@@ -0,0 +1,70 @@
+package com.ecommerce.aspect;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class LoggingAspect {
+
+	// @Around("execution(* *(..)) && @annotation(Loggable)")
+	@Around("execution(* com.ecommerce.web.controller..*(..))")
+	public void loggingController(ProceedingJoinPoint joinPoint)
+			throws Throwable {
+
+		joinPoint.proceed();
+		System.out
+				.println("************************************************************");
+		System.out.println("Service Around() is running!");
+		System.out.println(" ECommerce AOP Logging: "
+				+ joinPoint.getSignature().getName());
+		System.out
+				.println("************************************************************");
+	}
+
+	@After("execution(* com.ecommerce.rest.SearchService..*(..))")
+	public void loggingAfter(JoinPoint jp) throws Throwable {
+
+		System.out
+				.println("************************************************************");
+		System.out.println("Service After() is running!");
+
+		System.out
+				.println("************************************************************");
+	}
+
+	@Before("execution(* com.ecommerce.rest.SearchService..*(..))")
+	public void loggingBefore(JoinPoint jp) throws Throwable {
+
+		// joinPoint.proceed();
+		System.out
+				.println("************************************************************");
+		System.out.println("Service Before () is running!");
+
+		System.out
+				.println("************************************************************");
+	}
+
+	/*
+	 * @Around("execution(* com.ecommerce.rest.SearchService.getProducts(..))")
+	 * public void logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+	 * 
+	 * System.out.println("logAround() is running!");
+	 * System.out.println("hijacked method : " +
+	 * joinPoint.getSignature().getName());
+	 * System.out.println("hijacked arguments : " +
+	 * Arrays.toString(joinPoint.getArgs()));
+	 * 
+	 * System.out.println("Around before is running!"); joinPoint.proceed();
+	 * System.out.println("Around after is running!");
+	 * 
+	 * System.out.println("******");
+	 * 
+	 * }
+	 */
+}
